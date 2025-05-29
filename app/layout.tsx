31e83ae9247f31c118c5c85f5189d15100196292
Gihan sanjula <sanjulagihan94@gmail.com>
2025-05-29 15:08:23 +0530

    Add About and Users pages with basic content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          {""}
+          <Link href={"/about"}>about </Link>
+          <Link href={"/users"}>users</Link>
+        </div>
         {children}
       </body>
     </html>
---
+++ app/users/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>This is the User page</p>
+    </div>
+  );
+}
